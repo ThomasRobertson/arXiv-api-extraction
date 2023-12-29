a57d26e70907cda00ebe445fe6ec02c29282747e
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-12-29 01:33:52 +0100

    style: use Google style with pylint

--- test/test_connect_to_arxiv.py
+++ test/test_connect_to_arxiv.py
@@ -1,11 +1,12 @@
+"""Testing the connection to the arXiv API."""
 import unittest
 
-from src.connectToArXiv import getResponseFromAPI
+import connect_to_arxiv
 
 
 class TestArXivResult(unittest.TestCase):
     def setUp(self):
-        self.result = getResponseFromAPI("physics.optics", 1, 10)
+        self.result = connect_to_arxiv.GetResponseFromAPI("physics.optics", 1, 10)
 
     def test_notempty(self):
         self.assertTrue(self.result)
