2c81a05531fa583fccf6393da76f9615fd215451
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-12-29 18:45:46 +0100

    test: check numbers of entries

--- test/test_connect_to_arxiv.py
+++ test/test_connect_to_arxiv.py
@@ -5,10 +5,17 @@ from src import connect_to_arxiv
 
 class TestArXivResult(unittest.TestCase):
     def setUp(self):
-        self.result = connect_to_arxiv.GetResponseFromAPI(1, 10)
+        self.number_entries_requested = 10
+        self.result = connect_to_arxiv.GetResponseFromAPI(
+            1, self.number_entries_requested
+        )
 
-    def test_notempty(self):
+    def test_not_empty(self):
         self.assertTrue(self.result)
+
+    def test_number_of_entries(self):
+        number_of_headlines = len(self.result["entries"])
+        self.assertEqual(number_of_headlines, self.number_entries_requested)
 
     # def tearDown(self):
 
