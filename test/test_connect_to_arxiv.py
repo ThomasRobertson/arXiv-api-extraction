edda413a740c01bdd13799655eb444a748e149b5
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2024-01-03 23:28:32 +0100

    test: refactor using pytest

--- test/test_connect_to_arxiv.py
+++ test/test_connect_to_arxiv.py
@@ -1,24 +1,22 @@
+# pylint: disable=redefined-outer-name
 """Testing the connection to the arXiv API."""
-import unittest
+import pytest
 from src import connect_to_arxiv
 
-
-class TestArXivResult(unittest.TestCase):
-    def setUp(self):
-        self.number_entries_requested = 10
-        self.result = connect_to_arxiv.GetResponseFromAPI(
-            1, self.number_entries_requested
-        )
-
-    def test_not_empty(self):
-        self.assertTrue(self.result)
-
-    def test_number_of_entries(self):
-        number_of_headlines = len(self.result["entries"])
-        self.assertEqual(number_of_headlines, self.number_entries_requested)
-
-    # def tearDown(self):
+NUMBER_ENTRIES_REQUESTED = 10
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture
+def setup_data():
+    result = connect_to_arxiv.GetResponseFromAPI(1, NUMBER_ENTRIES_REQUESTED)
+    yield result
+
+
+def test_not_empty(setup_data):
+    assert setup_data
+
+
+def test_number_of_entries(setup_data):
+    result = setup_data
+    number_of_headlines = len(result["entries"])
+    assert number_of_headlines == NUMBER_ENTRIES_REQUESTED
