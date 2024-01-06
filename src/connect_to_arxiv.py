22b60f681cc5738d253675c2a40834b0a5265e1d
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2024-01-06 04:00:29 +0100

    refactor: use OAI-PHM to access metadata

--- src/connect_to_arxiv.py
+++ src/connect_to_arxiv.py
@@ -1,19 +1,27 @@
 """Connect to the arXiv API and return the results as entries of a FeedParserDict."""
-from typing import Any
-
-import feedparser
+from sickle import Sickle
 
 
-def GetResponseFromAPI(start: int, chunk_size: int) -> Any:
-    feed = feedparser.parse(
-        f"http://export.arxiv.org/api/query?search_query=start={start}&max_results={chunk_size}"
-    )
-    # if feed.status == 200:
-    #     numberOfHeadlines = len(feed["entries"])
+class BulkResponse:
+    """Access in bulk meta-data from arXiv using OAI-PHM."""
 
-    #     for i in range(0, numberOfHeadlines):
-    #         print(feed["entries"][i]["title"])
-    # else:
-    #     print("Something went wronge :", feed.status)
+    def __init__(self, from_date: str = "2012-12-12", until_date: str = "2012-12-19"):
+        self.sickle = Sickle("https://export.arxiv.org/oai2")
+        self.records = self.sickle.ListRecords(
+            False,
+            **{
+                "metadataPrefix": "oai_dc",
+                "from": {from_date},
+                "until": {until_date},
+            }
+        )
+        self.records = self.records.next()
+        self.namespaces = {
+            "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+            "oai_dc": "http://www.openarchives.org/OAI/2.0/oai_dc/",
+            "oai": "http://www.openarchives.org/OAI/2.0/",
+            "dc": "http://purl.org/dc/elements/1.1/",
+        }
 
-    return feed
+    def GetNextRecord(self) -> None:
+        self.records = self.records.next()
