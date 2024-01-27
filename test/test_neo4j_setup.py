09516554a8d43588e5c217a0f43710e878a95de3
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2024-01-27 01:45:32 +0100

    test: add record header to db and check read

--- test/test_neo4j_setup.py
+++ test/test_neo4j_setup.py
@@ -1,8 +1,11 @@
 # pylint: disable=redefined-outer-name
 import pytest
 from neo4j import GraphDatabase
+from src import connect_to_arxiv
+from src import fill_data_base
 
 URI = "neo4j://localhost:7687"
+
 
 @pytest.fixture(scope="session")
 def neo4j_driver():
@@ -10,9 +13,55 @@ def neo4j_driver():
     yield driver
     driver.close()
 
+
+@pytest.fixture(scope="session")
+def harvester():
+    harvester = connect_to_arxiv.ArXivHarvester()
+    return harvester
+
+
+@pytest.fixture(scope="session")
+def graph_db_connexion():
+    connexion = fill_data_base.GraphDBConnexion
+    return connexion
+
+
 def test_neo4j_connectivity(neo4j_driver):
-    try: 
+    try:
         neo4j_driver.verify_connectivity()
         assert True
     except Exception as e:
         pytest.fail(f"{e}")
+
+
+def test_neo4j_add_header(graph_db_connexion, harvester):
+    connexion = graph_db_connexion()
+    header = harvester.get_record_header()
+    connexion.add_record_header(header)
+    assert True
+
+
+def test_neo4j_read_header(neo4j_driver, harvester):
+    header = harvester.get_record_header()
+    driver = neo4j_driver
+    with driver.session() as session:
+        results = session.execute_read(read_header_tx, header)
+    for result in results:  # Use the results outside the transaction
+        value = result.value()
+        assert (
+            value["identifier"] == header["identifier"]
+            and value["datestamp"] == header["datestamp"]
+            and value["setSpec"] == header["setSpec"]
+        ), "Result does not match header"
+
+
+def read_header_tx(tx, header):
+    result_cursor = tx.run(
+        """
+        MATCH (r:Record {identifier: $identifier})
+        RETURN r
+        """,
+        identifier=header["identifier"],
+    )
+    results = [record for record in result_cursor]
+    return results
