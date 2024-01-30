3163b5fd9bc7d3019160d23816e22c45096b6972
ThomasRobertson <5249646+ThomasRobertson@users.noreply.github.com>
2024-01-30 13:35:48 +0100

    feat: add a --mock option to main

--- src/main.py
+++ src/main.py
@@ -1,26 +1,49 @@
+import argparse
 import connect_to_arxiv
 import fill_data_base
+import requests_mock
 
 
-def main():
-    harvester = connect_to_arxiv.ArXivHarvester()
+def main(mock=False):
+    m = requests_mock.Mocker()
+    if mock:
+        m.start()
+        with open("mock-response/request1.xml", "r", encoding="utf-8") as file:
+            data1 = file.read()
+        m.get(
+            "https://export.arxiv.org/oai2?verb=ListRecords&metadataPrefix=oai_dc&from=2021-03-20&until=2021-03-30&set=cs",
+            text=data1,
+        )
+
+        with open("mock-response/request2.xml", "r", encoding="utf-8") as file:
+            data2 = file.read()
+        m.get(
+            "https://export.arxiv.org/oai2?verb=ListRecords&resumptionToken=6965856|1001",
+            text=data2,
+        )
+
+        with open("mock-response/request3.xml", "r", encoding="utf-8") as file:
+            data3 = file.read()
+        m.get(
+            "https://export.arxiv.org/oai2?verb=ListRecords&resumptionToken=6965856|2001",
+            text=data3,
+        )
+
+    harvester = connect_to_arxiv.ArXivHarvester(
+        from_date="2021-03-20", until_date="2021-03-30", set_cat="cs"
+    )
     record = next(harvester.next_record())
-    first_harvest_first_record = harvester.get_record_header(record)
-    for i, record in enumerate(harvester.next_record(), 0):
-        if i == 999:
-            break
-    first_harvest_last_record = harvester.get_record_header(record)
-    record = next(harvester.next_record())
-    second_harvest_first_record = harvester.get_record_header(record)
+    header = harvester.get_record_header(record)
+    metadata = harvester.get_record_metadata(record)
+    dbConnexion = fill_data_base.GraphDBConnexion("neo4j://localhost:7687")
+    dbConnexion.add_record(header, metadata)
 
-    # checking for empty dict
-    assert bool(first_harvest_first_record) is True
-    assert bool(first_harvest_last_record) is True
-    assert bool(second_harvest_first_record) is True
-
-    assert first_harvest_first_record != first_harvest_last_record
-    assert first_harvest_first_record != second_harvest_first_record
+    if mock:
+        m.stop()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mock", action="store_true")
+    args = parser.parse_args()
+    main(mock=args.mock)
