9dda9cb1e5d19f1a598a6e9d2a4b1b23de6c0412
ThomasRobertson <5249646+ThomasRobertson@users.noreply.github.com>
2024-01-30 09:39:31 +0100

    test: mock the response from the API

--- test/conftest.py
+++ test/conftest.py
@@ -4,19 +4,39 @@ from neo4j import GraphDatabase
 import pytest
 from src import connect_to_arxiv, fill_data_base
 
-
 # https://export.arxiv.org/oai2?verb=ListRecords&metadataPrefix=oai_dc&from=2021-03-20&until=2021-03-23&set=cs
 
 
-@pytest.fixture(scope="session")
-def harvester() -> connect_to_arxiv.ArXivHarvester:
+@pytest.fixture(scope="function")
+def harvester(requests_mock) -> connect_to_arxiv.ArXivHarvester:
+    with open("mock-response/request1.xml", "r") as file:
+        data1 = file.read()
+    requests_mock.get(
+        "https://export.arxiv.org/oai2?verb=ListRecords&metadataPrefix=oai_dc&from=2021-03-20&until=2021-03-30&set=cs",
+        text=data1,
+    )
+
+    with open("mock-response/request2.xml", "r") as file:
+        data2 = file.read()
+    requests_mock.get(
+        "https://export.arxiv.org/oai2?verb=ListRecords&resumptionToken=6965856|1001",
+        text=data2,
+    )
+
+    with open("mock-response/request3.xml", "r") as file:
+        data3 = file.read()
+    requests_mock.get(
+        "https://export.arxiv.org/oai2?verb=ListRecords&resumptionToken=6965856|2001",
+        text=data3,
+    )
+
     harvester_return = connect_to_arxiv.ArXivHarvester(
         from_date="2021-03-20", until_date="2021-03-30", set_cat="cs"
     )
     return harvester_return
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def record(harvester):
     record_return = next(harvester.next_record())
     return record_return
@@ -24,7 +44,7 @@ def record(harvester):
 
 @pytest.fixture(scope="session")
 def neo4j_driver():
-    driver = GraphDatabase.driver("neo4j://localhost:7687")
+    driver = GraphDatabase.driver("neo4j://localhost:7687/neo4j")
     yield driver
     driver.close()
 
