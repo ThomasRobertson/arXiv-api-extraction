07063bb3df0f5aa48a3dc531fcdce7eeca4b2946
ThomasRobertson <5249646+ThomasRobertson@users.noreply.github.com>
2024-01-30 14:36:08 +0100

    refactor: remove old add_record_header() method

--- src/fill_data_base.py
+++ src/fill_data_base.py
@@ -12,21 +12,6 @@ class GraphDBConnexion:
     def __del__(self) -> None:
         self.driver.close()
 
-    def add_record_header(self, header: dict[str, str]) -> None:
-        with self.driver.session(database="neo4j") as session:
-            session.execute_write(self.record_header_tx, header)
-
-    def record_header_tx(self, tx, header: dict[str, str]):
-        record = tx.run(
-            """
-            MERGE (r:Record {identifier: $identifier, datestamp:$datestamp, setSpec:$setSpec})
-            RETURN r.identifier AS record
-            """,
-            identifier=header["identifier"],
-            datestamp=header["datestamp"],
-            setSpec=header["setSpec"],
-        )
-
     def add_record(self, header: dict[str, str], metadata: dict[str, list[str]]):
         with self.driver.session(database="neo4j") as session:
             session.execute_write(self.record_tx, header, metadata)
