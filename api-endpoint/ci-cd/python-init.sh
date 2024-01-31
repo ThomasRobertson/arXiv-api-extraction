3fe9fbab129c9dfb4ba486cb5ab8867decef3591
ThomasRobertson <5249646+ThomasRobertson@users.noreply.github.com>
2024-02-01 00:53:52 +0100

    ci: echo neo4j status on stdout

--- api-endpoint/ci-cd/python-init.sh
+++ api-endpoint/ci-cd/python-init.sh
@@ -16,11 +16,11 @@ export PYTHONPATH=$PWD
 
 # wait for Neo4j to be ready
 until curl http://neo4j:7474; do
-  >&2 echo "Neo4j is unavailable - sleeping"
+  echo "Neo4j is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Neo4j is up - executing command"
+echo "Neo4j is up - executing command"
 
 
 # launch pytest
